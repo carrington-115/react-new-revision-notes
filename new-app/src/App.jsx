fb4c6fd0fd38f585387d8d10321be60f693d5620
Mark Carrington <frumarkcarrington@gmail.com>
2024-01-23 19:48:35 +0530

    Changed the name of the images Added the books using an array of objects

--- new-app/src/App.jsx
+++ new-app/src/App.jsx
@@ -1,28 +1,60 @@
 import React from "react";
 import "./App.css";
 import Book from "./components/Book";
-import image from "./assets/images/51-78lFnmbL._SY445_SX342_.jpg";
+import image1 from "./assets/images/image1.jpg";
+import image2 from "./assets/images/image2.jpg";
+import image3 from "./assets/images/image3.jpg";
+
+const booksCollection = [
+  {
+    bookUrl: "amazon.in",
+    image: image1,
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    bookUrl: "amazon.in",
+    image: image2,
+    title: "IKIGAI",
+    author: "Hector Garcia and Francesc Miralles",
+  },
+  {
+    bookUrl: "amazon.in",
+    image: image3,
+    title: "The Power of Your Succonscious MIND",
+    author: "Joseph Murphy",
+  },
+];
 
 function BookList() {
   return (
     <div className="book-list-section">
       <h3 className="best-selling-title">Top 3 Best selling Books of 2023</h3>
       <section className="book-list-container">
+        {booksCollection.map((item, index) => (
+          <Book
+            key={index}
+            bookUrl={item.bookUrl}
+            image={item.image}
+            title={item.title}
+            author={item.author}
+          />
+        ))}
         <Book
           bookUrl="#"
-          image={image}
+          image={image1}
           title="Atomic Habits"
           author="James Clear"
         />
         <Book
           bookUrl="#"
-          image={image}
+          image={image2}
           title="Atomic Habits"
           author="James Clear"
         />
         <Book
           bookUrl="#"
-          image={image}
+          image={image3}
           title="Atomic Habits"
           author="James Clear"
         />
