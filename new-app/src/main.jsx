04d43872f72b1034f5a4b1c8e006561f699e0a0b
Mark Carrington <frumarkcarrington@gmail.com>
2024-01-23 10:18:48 +0530

    Learnt how to use React.DOM and React.Fragment

--- new-app/src/main.jsx
+++ new-app/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// using the React hook to create components
+// function Greetings() {
+//   return React.createElement("h2", {}, "hello world");
+// }
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    {/* <Greetings /> */}
   </React.StrictMode>
 );
