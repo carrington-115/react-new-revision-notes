ac849ec6ba08b0286eb144aa3b7682e367895d7e
Mark Carrington <frumarkcarrington@gmail.com>
2024-01-25 17:48:49 +0530

    using the useRef to prevent re-rendering when a state changes and to set an input active using the ref.current.focus() function which is also a part of vanilla js

--- new-app/src/components/ComponenWithUseRef.jsx
+++ new-app/src/components/ComponenWithUseRef.jsx
@@ -1,13 +1,20 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { formStyle, labelStyle, inputStyle, btnStyle } from "./UsingForms";
 
 function ComponenWithUseRef() {
   const nameRef = useRef(null);
+  const isMounted = useRef(false);
+  const [count, setCount] = useState(0);
 
   function handleSubmitData(e) {
     e.preventDefault();
     console.log("name: ", nameRef.current.value);
+    e.currentTarget.reset();
   }
+
+  useEffect(() => {
+    nameRef.current.focus();
+  }, []);
   return (
     <form style={formStyle} onSubmit={handleSubmitData}>
       <label style={labelStyle} htmlFor="name">
